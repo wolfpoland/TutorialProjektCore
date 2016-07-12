0502c9716f552ed5025d659151ec132e45e7a82d
Patryk <wolfpoland1@gmail.com>
2016-07-12 16:09:46 +0200

    Poczatkowy commit

---
+++ src/TutorialProjektCore/Controllers/HelloWorldController1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TutorialProjektCore.Controllers
+{
+    public class HelloWorldController1 : Controller
+    {
+        // GET: /<controller>/
+        public string Index()
+        {
+            return "To dziala z defaulta";
+        }
+
+
+        public string Welcome(string name,int numer=1)
+        {
+            return HtmlEncoder.Default.Encode($"Witaj {name}, id: {numer}");
+        }
+    }
+}
